8868804eefc6920aa8a4614c9953aca9f58ce4d4
WMDE-Fisch <christoph.fischer@wikimedia.de>
2015-06-03 11:30:39 +0200

    switched to lower case keyword #18

--- DeepCat.js
+++ DeepCat.js
@@ -7,7 +7,7 @@
  */
 
 (function () {
-	var keyString = 'deepCat:', maxDepth = 10, maxResults = 50, ajaxTimeout = 10000, deepCatSearchTerms;
+	var keyString = 'deepcat:', maxDepth = 10, maxResults = 50, ajaxTimeout = 10000, deepCatSearchTerms;
 	var requestUrl = '//tools.wmflabs.org/catgraph-jsonp/gptest1wiki_ns14/traverse-successors%20Category:{0}%20' + maxDepth + '%20' + maxResults;
 
 	switch ( mw.config.get( 'wgUserLanguage' ) ) {
