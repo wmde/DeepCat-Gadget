8367c4eaf234d787c9223c854446f2e1af79ebc3
WMDE-Fisch <christoph.fischer@wikimedia.de>
2015-06-04 10:15:20 +0200

    added detection for the wiki lang to compose cat graph url

--- DeepCat.js
+++ DeepCat.js
@@ -8,7 +8,9 @@
 
 (function () {
 	var keyString = 'deepcat:', maxDepth = 10, maxResults = 50, ajaxTimeout = 10000, deepCatSearchTerms;
-	var requestUrl = '//tools.wmflabs.org/catgraph-jsonp/gptest1wiki_ns14/traverse-successors%20Category:{0}%20' + maxDepth + '%20' + maxResults;
+	var contentLang = mw.config.get( 'wgContentLanguage' );
+	var requestUrl = '//tools.wmflabs.org/catgraph-jsonp/' + contentLang + 'wiki_ns14/traverse-successors%20Category:{0}%20' + maxDepth + '%20' + maxResults;
+
 
 	switch ( mw.config.get( 'wgUserLanguage' ) ) {
 		case 'de':
