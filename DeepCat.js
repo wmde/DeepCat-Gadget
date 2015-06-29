62d54464a0997e13ce1129f094bb3bc2716dc640
WMDE-Fisch <christoph.fischer@wikimedia.de>
2015-06-29 14:28:49 +0200

    style improvements at the head

--- DeepCat.js
+++ DeepCat.js
@@ -14,10 +14,15 @@ String.format = function () {
 	return s;
 };
 
-(function () {
-	var keyString = 'deepcat:', maxDepth = 10, maxResults = 50, ajaxTimeout = 10000, deepCatSearchTerms;
+( function( $, mw ) {
+	var keyString = 'deepcat:',
+		maxDepth = 10,
+		maxResults = 50,
+		ajaxTimeout = 10000,
+		deepCatSearchTerms;
 	var DBname = mw.config.get( 'wgDBname' );
-	var requestUrl = '//tools.wmflabs.org/catgraph-jsonp/' + DBname + '_ns14/traverse-successors%20Category:{0}%20' + maxDepth + '%20' + maxResults;
+	var requestUrl = '//tools.wmflabs.org/catgraph-jsonp/' + DBname +
+		'_ns14/traverse-successors%20Category:{0}%20' + maxDepth + '%20' + maxResults;
 
 	switch ( mw.config.get( 'wgUserLanguage' ) ) {
 		case 'de':
@@ -40,7 +45,7 @@ String.format = function () {
 			break;
 	}
 
-	$( function () {
+	$( function() {
 		$( '#searchform, #search' ).on( 'submit', function ( e ) {
 			var searchInput = $( this ).find( '[name="search"]' ).val();
 
