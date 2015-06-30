ad0e01770dccc5e60bcf4dd4500d4bf7186a52f7
Leszek Manicki <leszek.manicki@wikimedia.de>
2015-06-30 14:56:25 +0200

    Add tests for getSearchTerms

--- tests/DeepCat.tests.js
+++ tests/DeepCat.tests.js
@@ -6,4 +6,20 @@
 	QUnit.test( 'load DeepCat', function( assert ) {
 		assert.ok( DeepCat !== null, 'DeepCat is not null' );
 	} );
+
+	QUnit.test( 'getSearchTerms', function( assert ) {
+		assert.deepEqual( DeepCat.getSearchTerms( 'Foo'), [ 'Foo' ], 'getSearchTerms: a word not preceded by a keyword is a search term' );
+		assert.deepEqual( DeepCat.getSearchTerms( 'Foo Bar' ), [ 'Foo', 'Bar' ], 'getSearchTerms: each word not preceded by a keyword is a single search term' );
+		assert.deepEqual( DeepCat.getSearchTerms( 'deepcat:Foo'), [ 'deepcat:Foo' ], 'getSearchTerms: a word preceded by a keyword is a search term' );
+		assert.deepEqual( DeepCat.getSearchTerms( 'deepcat: Foo'), [ 'deepcat: Foo' ], 'getSearchTerms: spaces following the keyword do not stop matching the search term' );
+		assert.deepEqual( DeepCat.getSearchTerms( 'deepcat:Foo Bar'), [ 'deepcat:Foo', 'Bar' ], 'getSearchTerms: only a single word preceded by a keyword is a search term' );
+		assert.deepEqual( DeepCat.getSearchTerms( 'Foo deepcat:Bar'), [ 'Foo', 'deepcat:Bar' ], 'getSearchTerms: only a single word preceded by a keyword is a search term' );
+		assert.deepEqual( DeepCat.getSearchTerms( 'deepcat:Foo deepcat: Bar'), [ 'deepcat:Foo', 'deepcat: Bar' ], 'getSearchTerms: each DeepCat search term must be preceded by a keyword' );
+		assert.deepEqual( DeepCat.getSearchTerms( 'deepcat: Foo Bar deepcat:Baz'), [ 'deepcat: Foo', 'Bar', 'deepcat:Baz' ], 'getSearchTerms: only a single word preceded by a keyword is a search term' );
+		assert.deepEqual( DeepCat.getSearchTerms( 'deepcat:"Foo Bar"'), [ 'deepcat:"Foo Bar"' ], 'getSearchTerms: multiple words in quotes are considered a single DeepCat search term' );
+		assert.deepEqual( DeepCat.getSearchTerms( 'deepcat:"Foo Bar" Baz'), [ 'deepcat:"Foo Bar"', 'Baz' ], 'getSearchTerms: only multiple words in quotes are considered a single multi-word DeepCat search term' );
+		assert.deepEqual( DeepCat.getSearchTerms( 'deepcat:"Foo Bar"Baz'), [ 'deepcat:"Foo Bar"', 'Baz' ], 'getSearchTerms: multiple words in quotes are considered a single DeepCat search term' );
+		assert.deepEqual( DeepCat.getSearchTerms( 'deepcat:' ), [ 'deepcat:' ], 'getSearchTerms: empty DeepCat search terms are recognized' );
+		assert.deepEqual( DeepCat.getSearchTerms( 'Foo: Bar' ), [ 'Foo:', 'Bar' ], 'getSearchTerms: not every word followed by colon is considered a keyword' );
+	} );
 }( DeepCat, jQuery, QUnit ) );
