ec7b50bf7ba7c3cff2649b5aeb0ca3f8c8ccf4c5
Leszek Manicki <leszek.manicki@wikimedia.de>
2015-06-30 13:16:38 +0200

    Add mediaWiki mock

---
+++ tests/mediaWiki.mock.js
@@ -0,0 +1,15 @@
+mediaWiki = {
+	config: {
+		get: function() {
+			return 'foo';
+		}
+	},
+	messages: {
+		set: function() {}
+	},
+	util: {
+		getParamValue: function() {
+				return '';
+		}
+	}
+};
