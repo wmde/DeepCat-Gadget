a4835c190d07a02e1c2d4f282d8027f527243cd0
Gabriel Birke <gabriel.birke@wikimedia.de>
2015-07-06 10:49:50 +0200

    Add JS docstrings
    Add JS docstrings to computeResponses and computeErrors

--- DeepCat.js
+++ DeepCat.js
@@ -209,6 +209,13 @@
 		$( '#searchform' ).submit();
 	}
 
+	/**
+	 * Replace "deepcat:" search terms with "incategory:" terms from DeepCat response
+	 *
+	 * @param {array} responses Category search results
+	 * @param {array} newSearchTerms Original search terms provided by the user
+	 * @returns {array} Modified newSearchTerms
+	 */
 	DeepCat.computeResponses = function( responses, newSearchTerms ) {
 		var i,
 			userParameters,
@@ -241,6 +248,13 @@
 		return newSearchTerms;
 	};
 
+	/**
+	 * Add error messages to search form, remove erroneous search terms
+	 *
+	 * @param {array} errors Errors from DeepCat
+	 * @param {array} newSearchTerms Original search terms provided by the user
+	 * @returns {array} Modified newSearchTerms
+	 */
 	DeepCat.computeErrors = function( errors, newSearchTerms ) {
 		var i,
 			userParameters,
